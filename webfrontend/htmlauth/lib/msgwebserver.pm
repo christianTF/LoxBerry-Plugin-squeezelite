9686feb108866ff6a9d80acc05e5f750854b0adb
Christian Fenzl <fenzl@t-r-t.at>
2022-01-04 06:07:55 +0100

    Revert "First working implementation of MSG interface :-)"
    This reverts commit 4b97cff0ef260f2b17cebcd40503c02674b56feb.

--- webfrontend/htmlauth/lib/msgwebserver.pm
+++ webfrontend/htmlauth/lib/msgwebserver.pm
@@ -8,6 +8,8 @@ package MSGWEB;
 # Config file ~/config/plugins/squeezelite/plugin_squeezelite.cfg
 # needs following instance:
 #
+#
+#
 # [MSG]
 # Activated=True
 # FMS1_Activated=True
@@ -38,7 +40,6 @@ my $version = "1.0.6.2";
 my $lmscommand;
 my $fl; # is the log object
 my $fms;
-my $playerstates;
 
 # Starting routine for the threads
 # Parameter is fmsid as number (1,2,....) like in the config file
@@ -65,6 +66,13 @@ sub start_fmsweb
 	$fms = $main::msg_servers{$fmsid};
 	# e.g. $fms->{host} is the host:port
 
+	# # Creating pid
+	# my $pidfile = "/run/shm/msgwebserver.$$";
+	# open(my $fh, '>', $pidfile);
+	# print $fh "$$";
+	# close $fh;
+	# $fl->DEB("$$ My PID is: $$");
+
 	$fl->INF("$$ This webserver runs for Fake Musicserver (FMS) $fmsid on port " . $fms->{LocalWebPort});
 
 	###############################################
@@ -84,43 +92,39 @@ sub start_fmsweb
 		
 		$fl->DEB("Received COMMAND $command $value for ZONE $zone");
 
-		# Define playerstates here because we have to modify the state below to 
-		# sync the Loxone WebGUI 
-		$playerstates = \%main::playerstates; 
-
 		switch ($command) {
 			case 'play'  { 
 				$lmscommand = "play";
-				$playerstates->{$player}->{Mode} = 1;
+				 &change_state($player,'Mode','1');
 			}
-			case 'resume' { 
+					case 'resume' { 
 				$lmscommand = "play";
-				$playerstates->{$player}->{Mode} = 1;
+				&change_state($player,'Mode','1');
 			}
-			case 'stop' { 
+					case 'stop' { 
 				$lmscommand = "stop";
-				$playerstates->{$player}->{Mode} = 0;
+				&change_state($player,'Mode','0');
 			}
-			case 'pause' { 
+					case 'pause' { 
 				$lmscommand = "pause";
-				$playerstates->{$player}->{Mode} = 0;
+				&change_state($player,'Mode','0');
 			}
-			case 'shuffle' { 
+					case 'shuffle' { 
 				$lmscommand = "playlist shuffle $value";
-				$playerstates->{$player}->{Shuffle} = $value;
+				&change_state($player,'Shuffle',$value);
 			}
-			case 'repeat' { 
+					case 'repeat' { 
 				$lmscommand = "playlist repeat $value";
-				$playerstates->{$player}->{Repeat} = $value;
+				&change_state($player,'Repeat',$value);
 			}
-			case 'volume' { 
-				$lmscommand = "mixer volume $value";
-				$playerstates->{$player}->{Volume} = $value;
 			}
-		}
 
+		#
 		# Send to LMS tcp queue
 		# The queue is managed by the main thread
+		#
+		# print $tcpout_sock "$player $lmscommand\n";
+		
 		{
 			# During lock, other threads will wait for release 
 			threads::shared::lock(@main::tcpout_queue);
@@ -128,6 +132,7 @@ sub start_fmsweb
 			# Lock is released automatically after leaving the scope of the block
 		}
 	
+		
 		# Read Player States and create output
 		my $state = &create_state($zone);
 		
@@ -143,19 +148,11 @@ sub start_fmsweb
 		my $c = shift;
 		my $zone = $c->stash('zone');
 
-		# Check peer information
-		my $address = $c->tx->remote_address;
-		my $port    = $c->tx->remote_port;
-		
-		$fl->DEB("Request from $address\:$port");
-
 		# Read Player States and create output
 		my $state = &create_state($zone);
 		
 		# Render in UTF8
 		utf8::decode($state);
-		utf8::decode($state);
-		$c->res->headers->header('Content-Type' => 'application/json');
 		$c->render(text => $state);
 
 	};
@@ -179,19 +176,21 @@ sub create_state {
 	my $zone = shift;
 	my $player = $fms->{zone}->{$zone};
 
-	if (! $playerstates ) {
-		$playerstates = \%main::playerstates; 
-		# KISS (copy the ref to not need the write the main:: package qualifier
-		# $playerstates is not a copy of data. It is the actual state of lms2udp
-		$fl->DEB("create_state: playerstates not yet defined. Will read them from main::playerstates");
-	}
+	my $playerstates = \%main::playerstates; 
+	# KISS (copy the ref to not need the write the main:: package qualifier
+	# $playerstates is not a copy of data. It is the actual state of lms2udp
+	
+	$fl->DEB("create_state playerstates\n" . Data::Dumper::Dumper($playerstates));
+	
+	# &read_states();
 
-	$fl->DEB("Creating State for Player " . $player);
-
+	$fl->DEB("Zone is $zone, Player is $player");
+	
 	if( !zone_available($zone) ) {
 		$fl->ERR("Zone $zone does not match to a valid player mac in LMS");
 		return;
 	}
+
 	
 	# Recalculate Mode
 	my $mode;
@@ -201,13 +200,14 @@ sub create_state {
 	else { $mode = "buffer"; }
 
 	$fl->DEB("Mode is " . $playerstates->{$player}->{Mode});
+
 	
 	# Create json
 	my %response = (
 		'player' => {
 			'id' => $player,
 			'mode' => $mode,
-			'time' => int($playerstates->{$player}->{time_fuzzy}*1000),
+			'time' => $playerstates->{$player}->{time_fuzzy}*1000,
 			'volume' => $playerstates->{$player}->{volume},
 			'repeat' => $playerstates->{$player}->{Repeat},
 			'shuffle' => $playerstates->{$player}->{Shuffle}
@@ -217,18 +217,129 @@ sub create_state {
 			'album' => $playerstates->{$player}->{Album},
 			'id' => $player,
 			'artist' => $playerstates->{$player}->{Artist},
-			'duration' => int($playerstates->{$player}->{Duration}*1000),
+			'duration' => $playerstates->{$player}->{Duration}*1000,
 			'image' => $playerstates->{$player}->{Cover}
 		}
 	);
-	undef $playerstates;
-
-	#$fl->DEB("Response state data:\n" . Data::Dumper::Dumper(\%response)); 
+	$fl->DEB("Response state data:\n" . Data::Dumper::Dumper(\%response)); 
 	my $jsonresponse = JSON::PP::encode_json(\%response);
-
 	return($jsonresponse);
 
 }
+
+#
+# Read states from tmpfs datafile (created by lms2udp.pl)
+# 
+sub read_states {
+
+	# CF: Zugriff auf die Daten direkt mittels $playerstates->{$mac}->{...Attribut...}
+
+	$fl->DEB("Sub read_states - OBSOLETE");
+	# my $jsonobj = LoxBerry::JSON->new();
+	# my $data = $jsonobj->open(filename => $datafile, readonly => 1);
+	# $playerstates = $data->{'States'};
+	return (1);
+}
+
+#
+# Change states in tmpfs datafile (created by lms2udp.pl)
+# 
+sub change_state {
+
+	# CF: Weiß nicht genau, wofür das ist?
+
+
+	$fl->DEB("Sub change_state");
+	# my $player = shift;
+	# my $key = shift;
+	# my $value = shift;
+
+	# $fl->INF("State of $player changed: $key is now $value");
+
+	# my $jsonobj = LoxBerry::JSON->new();
+	# my $data = $jsonobj->open(filename => $datafile, readonly => 0);
+	# $data->{States}->{$player}->{$key} = $value;
+	# my $saved = $jsonobj->write();
+	# return (1);
+
+}
+
+# #
+# # Read plugin config
+# #
+# sub read_config {
+
+	# # Check existance of config file
+	# if (! (-e $cfgfilename)) {
+		# LOGCRIT "Squeezelite Player Plugin MSGWEBSERVER configuration does not exist. Terminating.\n";
+		# LOGTITLE "MSG Webserver stopped (no configuration)";
+		# unlink $pidfile;
+		# exit(0);
+	# }
+	
+	# $fl->INF("Reading Plugin config $cfgfilename");
+		
+	# # Read the Plugin config file 
+	# $cfg = new Config::Simple($cfgfilename);
+
+	# # guest port of lms2udp
+	# $berrytcpport = $cfg->param("LMS2UDP.berrytcpport");
+	# if (!$berrytcpport) { $berrytcpport = 9092; };
+
+	# # Read MSI config from config
+	# $fmsid_activated = $cfg->param("MSG.Activated");
+	# if ( $fmsid_activated ) {
+		# $fl->INF "MSG is ENABLED";
+		# $port = $cfg->param("MSG.Musicserver$fmsid\_Port");
+		# if (!$port) {$port = "8091"};
+		# for ( my $i=1; $i<=30; $i++ ) { # 30 Zones max
+			# if ( $cfg->param("MSG.Musicserver$fmsid\_Z$i") ) {
+				# $fmsid_players{ $i } = $cfg->param("MSG.Musicserver$fmsid\_Z$i");
+				# $fl->INF "MSG ZONE $i is PLAYER " . $cfg->param("MSG.Musicserver$fmsid\_Z$i");
+			# }
+		# }
+	# }
+
+# }
+
+#
+# Create Out Socket
+# Params: $socket, $port, $proto (tcp, udp), $remotehost
+# Returns: $socket
+#
+# sub create_out_socket 
+# {
+	# my ($socket, $port, $proto, $remotehost) = @_;
+	
+	# my %params = (
+		# PeerHost  => $remotehost,
+		# PeerPort  => $port,
+		# Proto     => $proto,
+		# Blocking  => 0
+	# );
+	
+	# if ($proto eq 'tcp') {
+		# $params{'Type'} = SOCK_STREAM;
+	# } elsif ($proto eq 'udp') {
+		# # $params{'LocalAddr'} = 'localhost';
+	# }
+	# if($socket) {
+		# close($socket);
+	# }
+		
+	# $socket = IO::Socket::INET->new( %params )
+		# or die "Couldn't connect to $remotehost:$port : $@\n";
+	# sleep (0.02);
+	# if ($socket->connected) {
+		# $fl->OK("Created $proto out socket to $remotehost on port $port");
+	# } else {
+		# $fl->WARN("Socket to $remotehost on port $port seems to be offline - will retry");
+	# }
+	# IO::Socket::Timeout->enable_timeouts_on($socket);
+	# $socket->read_timeout(2);
+	# $socket->write_timeout(2);
+	# return $socket;
+# }
 
 # Checks if the requested zone has an available mac defined
 # Parameter is zone number
@@ -240,7 +351,7 @@ sub zone_available
 		return;
 	}
 	
-	#$fl->DEB(Data::Dumper::Dumper($fms));
+	$fl->DEB(Data::Dumper::Dumper($fms));
 	
 	my $player = $fms->{zone}->{$zone};
 	if (!$player) {
@@ -248,7 +359,7 @@ sub zone_available
 		return;
 	}
 	
-	#$fl->DEB("Playerstates:\n" . Data::Dumper::Dumper(\%main::playerstates));
+	$fl->DEB("Playerstates:\n" . Data::Dumper::Dumper(\%main::playerstates));
 	
 	
 	if( !defined $main::playerstates{$player} ) {
